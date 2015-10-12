0d21dcd25f4313e8feff388884e5de43433f6347
Trung Lê <trung.le@ruby-journal.com>
2015-10-12 23:38:01 +1100

    Add institutions/locations import script

--- lib/cricos_api/repositories/institution_repository.rb
+++ lib/cricos_api/repositories/institution_repository.rb
@@ -6,4 +6,8 @@ class InstitutionRepository
       where(provider_code: code)
     end.first
   end
+
+  def self.find_or_initialize_by_provider_code(code)
+    find_by_provider_code(code) || Institution.new(provider_code: code)
+  end
 end
--- lib/cricos_api/repositories/location_repository.rb
+++ lib/cricos_api/repositories/location_repository.rb
@@ -1,3 +1,14 @@
 class LocationRepository
   include Lotus::Repository
+
+  def self.find_by_institution_id_and_location_id(institution_id, location_id)
+    query do
+      where(institution_id: institution_id, location_id: location_id)
+    end.first
+  end
+
+  def self.find_or_initialize_by_institution_id_and_location_id(institution_id, location_id)
+    find_by_institution_id_and_location_id(institution_id, location_id) ||
+      Location.new(institution_id: institution.id, location_id: location_id)
+  end
 end
