9e76af1353c676a71948ef61b841ba5f95ac458d
Trung Lê <trung.le@ruby-journal.com>
2015-10-14 20:45:00 +1100

    Add contact_officers import logic

--- lib/cricos_api/repositories/contact_officer_repository.rb
+++ lib/cricos_api/repositories/contact_officer_repository.rb
@@ -1,3 +1,14 @@
 class ContactOfficerRepository
   include Lotus::Repository
+
+  def self.find_by_institution_id_and_name(institution_id, name)
+    query do
+      where(institution_id: institution_id, name: name)
+    end.first
+  end
+
+  def self.find_or_initialize_by_institution_id_and_name(institution_id, name)
+    find_by_institution_id_and_name(institution_id, name) ||
+      ContactOfficer.new(institution_id: institution_id, name: name)
+  end
 end
