215f0abf134ce84990d359ca868d14f5fc9fc368
Trung Lê <trung.le@ruby-journal.com>
2015-10-27 23:54:03 +1100

    Add extra test case for serializing an array of institutions

--- spec/api_v1/serializers/institution_serializer_spec.rb
+++ spec/api_v1/serializers/institution_serializer_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe InstitutionSerializer do
+describe JSONAPI::Serializer do
   describe '.serialize' do
     let!(:institution) {
       Institution.new(
@@ -17,9 +17,21 @@ describe InstitutionSerializer do
         contact_officers: [])
     }
 
-    it 'returns serialized JSON:API presentation' do
-      output = JSONAPI::Serializer.serialize(institution)
-      expect(output).to eq({"data"=>{"id"=>"1", "type"=>"institutions", "attributes"=>{"id"=>1, "provider-id"=>"CODE_X", "provider-code"=>"X-MEN", "trading-name"=>"The Xavier College", "name"=>"The X-Men School", "type"=>"Government", "total-capacity"=>5000, "postal-address"=>"X-Men Office\\nBox 123\\nCANBERRA\\nACT 2500", "website"=>"http://x-men.com/", "locations"=>[], "contact-officers"=>[]}, "links"=>{"self"=>"/institutions/1"}}})
+    context 'when object is Institution' do
+      it 'returns serialized JSON:API presentation' do
+        output = JSONAPI::Serializer.serialize(institution)
+        expect(output).to eq({"data"=>{"id"=>"1", "type"=>"institutions", "attributes"=>{"id"=>1, "provider-id"=>"CODE_X", "provider-code"=>"X-MEN", "trading-name"=>"The Xavier College", "name"=>"The X-Men School", "type"=>"Government", "total-capacity"=>5000, "postal-address"=>"X-Men Office\\nBox 123\\nCANBERRA\\nACT 2500", "website"=>"http://x-men.com/", "locations"=>[], "contact-officers"=>[]}, "links"=>{"self"=>"/institutions/1"}}})
+      end
     end
+
+    context 'when object is an array of Institutions' do
+      let!(:institutions) { [institution] }
+
+      it 'returns serialized JSON:API presentation' do
+        output = JSONAPI::Serializer.serialize(institutions, is_collection: true)
+        expect(output).to eq({"data"=>[{"id"=>"1", "type"=>"institutions", "attributes"=>{"id"=>1, "provider-id"=>"CODE_X", "provider-code"=>"X-MEN", "trading-name"=>"The Xavier College", "name"=>"The X-Men School", "type"=>"Government", "total-capacity"=>5000, "postal-address"=>"X-Men Office\\nBox 123\\nCANBERRA\\nACT 2500", "website"=>"http://x-men.com/", "locations"=>[], "contact-officers"=>[]}, "links"=>{"self"=>"/institutions/1"}}]})
+      end
+    end
+
   end
 end
